746bd32207005c74725b803150de6d20ad99218d
alexw3f <37415622+alexw3f@users.noreply.github.com>
2024-01-23 13:40:37 +0100

    Create config-screen-2.js
    add config for 2nd screen

---
+++ config-screen-2.js
@@ -0,0 +1,25 @@
+{
+  "settingsReloadIntervalMinutes": 1,
+  "fullscreen": true,
+  "autoStart": true,
+  "lazyLoadTabs": false,
+  "closeExistingTabs": true,
+  "websites": [
+    {
+      "url": "https://polkadot.js.org/apps/#/explorer",
+      "duration": 30
+    },
+    {
+      "url": "https://polkadot.js.org/apps/#/explorer/forks",
+      "duration": 30
+    },
+    {
+      "url": "https://telemetry.polkadot.io/#/0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3",
+      "duration": 30
+    },
+    {
+      "url": "https://polkadot.subscan.io/xcm_dashboard",
+      "duration": 30
+    }
+  ]
+}
