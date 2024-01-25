ecaeaa8d1a0cf8981997712b53e289c65c66c654
alexw3f <37415622+alexw3f@users.noreply.github.com>
2024-01-25 11:46:49 +0100

    Update config-screen-1.js

--- config-screen-1.js
+++ config-screen-1.js
@@ -9,11 +9,11 @@
       "url": "https://metabase.data.paritytech.io/question/644-relay-chain-number-of-successful-extrinsic-signers?chain=kusama&chain=polkadot&timestamp=past12months",
       "duration": 30
       // Relay Chain: Number of Successful Extrinsic Signers 1 year
-    }// ,
-   //  {
-    //   "url": "https://metabase.data.paritytech.io/question/1139-aggregated-number-of-transfers-donut?date=past1years",
-     //  "duration": 30
-      // USD Transfers 1 year
+    },
+   {
+     "url": "https://metabase.data.paritytech.io/question/643-relay-chain-number-of-fee-payers?chain=kusama&chain=polkadot&timestamp=past12months",
+     "duration": 30
+      // Relay Chain: Number of Fee Payers 1 year
     // }
   ]
 }
