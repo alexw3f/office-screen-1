2053711b085cbd18a45689db51a93bff09e223c9
alexw3f <37415622+alexw3f@users.noreply.github.com>
2024-01-25 15:58:58 +0100

    Update config-screen-1.js

--- config-screen-1.js
+++ config-screen-1.js
@@ -6,7 +6,7 @@
   "closeExistingTabs": true,
   "websites": [
     {
-      "url": "https://metabase.data.paritytech.io/question/644-relay-chain-number-of-successful-extrinsic-signers?chain=kusama&chain=polkadot&timestamp=past12months",
+      "url": "https://metabase.data.paritytech.io/question/1738-number-of-active-accounts-substrate-evm",
       "duration": 30
     },
    {
