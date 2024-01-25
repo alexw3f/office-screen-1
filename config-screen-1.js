5fbbbed4480c7dfc40b85e4b0ac384c4d6068cfd
alexw3f <37415622+alexw3f@users.noreply.github.com>
2024-01-25 12:00:38 +0100

    Update config-screen-1.js

--- config-screen-1.js
+++ config-screen-1.js
@@ -8,12 +8,10 @@
     {
       "url": "https://metabase.data.paritytech.io/question/644-relay-chain-number-of-successful-extrinsic-signers?chain=kusama&chain=polkadot&timestamp=past12months",
       "duration": 30
-      // Relay Chain: Number of Successful Extrinsic Signers 1 year
     },
    {
      "url": "https://metabase.data.paritytech.io/question/643-relay-chain-number-of-fee-payers?chain=kusama&chain=polkadot&timestamp=past12months",
      "duration": 30
-      // Relay Chain: Number of Fee Payers 1 year
     }
   ]
 }
