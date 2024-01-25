abd3ef02aaa363d11a4c8e4d253af6b25b42f4fa
alexw3f <37415622+alexw3f@users.noreply.github.com>
2024-01-25 11:43:36 +0100

    Update config-screen-1.js
    change sites

--- config-screen-1.js
+++ config-screen-1.js
@@ -6,12 +6,14 @@
   "closeExistingTabs": true,
   "websites": [
     {
-      "url": "https://metabase.data.paritytech.io/dashboard/132-transfers-dashboard?relative_date=past30days",
+      "url": "https://metabase.data.paritytech.io/question/644-relay-chain-number-of-successful-extrinsic-signers?chain=kusama&chain=polkadot&timestamp=past12months",
       "duration": 30
-    },
-    {
-      "url": "https://metabase.data.paritytech.io/dashboard/173-website-analytics-dashboard?tab=66-general-overview&date_filter=past1months~&domain=polkadot.network",
-      "duration": 30
-    }
+      // Relay Chain: Number of Successful Extrinsic Signers 1 year
+    }// ,
+   //  {
+    //   "url": "https://metabase.data.paritytech.io/question/1139-aggregated-number-of-transfers-donut?date=past1years",
+     //  "duration": 30
+      // USD Transfers 1 year
+    // }
   ]
 }
