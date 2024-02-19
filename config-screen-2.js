a430a0cf593ecdd07c15d3b4af0fdcca1cedc8c5
alexw3f <37415622+alexw3f@users.noreply.github.com>
2024-02-19 14:11:34 +0100

    Update config-screen-2.js

--- config-screen-2.js
+++ config-screen-2.js
@@ -6,6 +6,10 @@
   "closeExistingTabs": true,
   "websites": [
     {
+      "url": "https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Frpc-polkadot.luckyfriday.io#/explorer",
+      "duration": 30
+    },
+    {
       "url": "https://telemetry.polkadot.io/#/0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3",
       "duration": 30
     },
