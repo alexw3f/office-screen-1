1854dab8628bfd75f63c1d6e9c1b924d6d9b48c5
alexw3f <37415622+alexw3f@users.noreply.github.com>
2025-01-23 11:40:29 +0100

    Update config-screen-1.js

--- config-screen-1.js
+++ config-screen-1.js
@@ -5,7 +5,17 @@
   "lazyLoadTabs": false,
   "closeExistingTabs": true,
   "websites": [
-   {
+    {
+     "url": "https://test.metabase.data.paritytech.io/question/853-polkadot-monthly-active-addresses-by-parachain-and-relay-chain",
+     "duration": 30,
+     "tabReloadIntervalSeconds": 0
+    },
+    {
+     "url": "https://test.metabase.data.paritytech.io/question/852-polkadot-monthly-signed-transactions-by-parachain-and-relay-chain",
+     "duration": 30,
+     "tabReloadIntervalSeconds": 0
+    },
+    {
      "url": "https://www.developerreport.com/ecosystems/polkadot",
      "duration": 30,
      "tabReloadIntervalSeconds": 0
